dea204f424bda2bf11a5da2375c1227fe82a0afd
Francisco Burgos Santibañez <f.burgoss1589@gmail.com>
2021-12-24 14:18:33 -0300

    subiendo el archivo

---
+++ main.js
@@ -0,0 +1,26 @@
+var cursos = 'programacion';
+
+
+// usuario ingresa su nombre y lo alamecamos en prompt
+let nombre = prompt('Ingresa tu nombre')    
+
+
+// usuario ingresa su nombre y lo alamecamos en prompt
+let mes = prompt('Ingresa el mes que prefieres comenzar clases') 
+
+
+// usuario ingresa el dia y lo alamecamos en prompt
+let dia = prompt('Ingresa el dia del 1 al 30 que prefieres comenzar las clases')  
+
+
+
+//se lanza un mensaje de bienvenida alert
+let mensajebienbenida = 'Bienbenid@ a Coderhouse  '
+
+// imprimo por consola el dia y mes de inicio de clases, ademas de un mennsaje de bienbenida
+console.log(dia  +  mes  +  ' es tu comienzo de clases ' + nombre)
+console.log(mensajebienbenida + nombre + ' , gracias por elegirnos  ')
+
+
+// al fianlizar el ingreso de datos lanza un alert con un pequeño mensaje navideño
+alert('Buen dia y que tengas una muy feliz navidad de parte de todo equipo de CODERHOUSE !!')
