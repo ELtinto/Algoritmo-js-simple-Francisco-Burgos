5036561b10dffabedf3880c7b470b8a2620f5363
Francisco Burgos Santibañez <f.burgoss1589@gmail.com>
2021-12-29 01:21:47 -0300

    algoritmo con condicional

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-var cursos = 'programacion';
+//ingreso un numero para verificar si este es menor o mayor a 1000
+
+let numero = Number (prompt('ingrese un numero'));
 
 
-// usuario ingresa su nombre y lo alamecamos en prompt
-let nombre = prompt('Ingresa tu nombre')    
+if(numero>=1000){
+    console.log('el numero ingresado es mayor a 1.000');
+    alert('el numero ingresado es mayor a 1.000');
 
-
-// usuario ingresa su nombre y lo alamecamos en prompt
-let mes = prompt('Ingresa el mes que prefieres comenzar clases') 
-
-
-// usuario ingresa el dia y lo alamecamos en prompt
-let dia = prompt('Ingresa el dia del 1 al 30 que prefieres comenzar las clases')  
-
-
-
-//se lanza un mensaje de bienvenida alert
-let mensajebienbenida = 'Bienbenid@ a Coderhouse  '
-
-// imprimo por consola el dia y mes de inicio de clases, ademas de un mennsaje de bienbenida
-console.log(dia  +  mes  +  ' es tu comienzo de clases ' + nombre)
-console.log(mensajebienbenida + nombre + ' , gracias por elegirnos  ')
-
-
-// al fianlizar el ingreso de datos lanza un alert con un pequeño mensaje navideño
-alert('Buen dia y que tengas una muy feliz navidad de parte de todo equipo de CODERHOUSE !!')
+}
+else{
+    console.log('el numero ingresado es menor a 1.000');
+    alert('el numero ingresado es menor a 1.000');
+}
